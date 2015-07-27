37bc67f5fc8f0ad77efbd0cea73991bbc9bcc0f2
andyduino <andyduino@gmail.com>
2015-07-27 08:16:17 +0700

    build jsonStr for AM2301
    Modify MQTT_for_SIM900.ino
    Add dht library
    build json function

--- MQTT-for-SIM900/MQTT-for-SIM900.ino
+++ MQTT-for-SIM900/MQTT-for-SIM900.ino
@@ -9,13 +9,45 @@
  disabled GPS for simplicity
  sending "static" information for simplicity
 */
+
+
 #include <mqtt.h>
+#include <Math.h>
+//#include <SoftwareSerial.h>
+#include <String.h>
+#include "DHT.h"
 
 /* #include <SoftwareSerial.h>
 #include "TinyGPS.h"
 TinyGPS gps;
 */
+// initialize the library with the numbers of the interface pins
+//variable for Water temperature
+// which analog pin to connect
+#define THERMISTORPIN A2         
+// resistance at 25 degrees C
+#define THERMISTORNOMINAL 10000      
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25   
+// how many samples to take and average, more takes longer
+// but is more 'smooth'
+#define NUMSAMPLES 5
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
+// the value of the 'other' resistor
+#define SERIESRESISTOR 10000
 
+int samples[NUMSAMPLES];
+
+
+#define DHTPIN A0    //Analog Pin 0 for AM2301
+#define DHTTYPE DHT21// AM2301 
+
+DHT dht(DHTPIN, DHTTYPE);
+float Air_Temp = 0.0; //AM2301 temparature in celcus
+float Air_Humidity = 0.0; // AM2301 Humidity 
+int ldr = 1;             //analog pin to which LDR is connected
+int ldr_value = 0;        //variable to store LDR values
 
 // GPS parser for 406a
 #define BUFFSIZ 90 // plenty big
@@ -42,36 +74,39 @@ boolean gprsReady = false;
 boolean mqttSent = false;
 int sentCount = 0;
 // SoftwareSerial gpss(1, 3);
+
+
+
 void setup() {
  pinMode(13, OUTPUT);
  pinMode(14, OUTPUT); // GSM
  digitalWrite(0, HIGH);
 
 
- Serial.begin(9600);
- Serial1.begin(19200);
+ Serial.begin(9600); //debug
+ Serial1.begin(19200); // GPRS module
 // gpss.begin(9600);
+ dht.begin();
 
-Serial.println("Hello");
- digitalWrite(14, HIGH); // GSM ON
- delay(1000);
- digitalWrite(14, LOW); // GSM ON
- delay(1000);
 }
 void loop(){
-
+ delay(20000);// wait for GPRS getting stable
+ get_AM2301_Data();
+ 
  Serial.println("Checking if GPRS is ready");
  gprsReady = isGPRSReady();
-
+ 
  if (gprsReady == true){
  Serial.println("GPRS Ready");
+ String json = buildJson();
+ char jsonStr[200];
+ json.toCharArray(jsonStr,200);
+    
  // Change the IP and Topic.
  /* The arguments here are:
  clientID, IP, Port, Topic, Message
  */
-sendMQTTMessage("agrinode", "iot.eclipse.org", "1883", "agrinode1", "Cao Hoang Tien _ MQTT Client Test");
-
-
+sendMQTTMessage("agrinode", "iot.eclipse.org", "1883", "agrinode1",jsonStr);
  }
 
  delay(10000);
@@ -90,6 +125,7 @@ uint32_t parsedecimal(char *str) {
  }
  return d;
 }
+
 void readline() {
  /*char c;
  buffidx = 0; // start at begninning
@@ -127,6 +163,7 @@ boolean isGPRSReady(){
  return false;
  }
 }
+
 void sendMQTTMessage(char* clientId, char* brokerUrl, char* brokerPort, char* topic, char* message){
  Serial1.println("AT"); // Sends AT command to wake up cell phone
  Serial.println("send AT to wake up GPRS");
@@ -182,3 +219,36 @@ void sendMQTTMessage(char* clientId, char* brokerUrl, char* brokerPort, char* to
  delay(2000);
 }
 
+//get AM2301 data function
+void get_AM2301_Data() { 
+    Serial.println("Read data"); 
+   Air_Humidity = dht.readHumidity();
+   Air_Temp = dht.readTemperature();
+   
+   if (isnan(Air_Humidity) || isnan(Air_Temp)) {
+    Serial.println("Failed to read from DHT sensor");
+    Air_Humidity=0;
+    Air_Temp=0;
+    return;
+  }
+}
+  
+String buildJson() {
+  String data = "{";
+  data+="\n";
+  data+= "\"d\": {";
+  data+="\n";
+  data+="\"myName\": \"Arduino DHT11\",";
+  data+="\n";
+  data+="\"Air Temperature\": ";
+  data+=(int)Air_Temp;
+  data+= ",";
+  data+="\n";
+  data+="\"Air Humidity\": ";
+  data+=(int)Air_Humidity;
+  data+="\n";
+  data+="}";
+  data+="\n";
+  data+="}";
+  return data;
+}
